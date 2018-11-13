495398d6717396921decf2167148627a39d7ddd4
CodyBagwandeen <cbagwandeen@stuy.edu>
2018-11-13 17:32:22 -0500

    updated to allow testing to be faster

--- words.bat
+++ words.bat
@@ -1,3 +1,21 @@
-javac *.java
 java driver
+java driver 15 15 smash.txt
+java driver 15 15 smash.txt 23
+java driver 15 15 smash.txt 23 key
+
+java driver 15 15
+java driver a 15 smash.txt 23 key
+java driver 15 b smash.txt 23 key
+java driver 15 15 smash.txt a key
+java driver 15 15 smash.xt 23 key
+
 java WordSearch
+java WordSearch 15 15 smash.txt
+java WordSearch 15 15 smash.txt 50
+java WordSearch 15 15 smash.txt 50 key
+
+java WordSearch 15 15
+java WordSearch 15 a smash.txt 50 key
+java WordSearch b 15 smash.txt 50 key
+java WordSearch 15 15 smash.txt a key
+java WordSearch 15 15 smash.tt 50 key
