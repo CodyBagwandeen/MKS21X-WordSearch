bb6c03c2f70c2853973bde79139d8a029acfa4ed
CodyBagwandeen <cbagwandeen@stuy.edu>
2018-11-18 22:09:46 -0500

    just incase :)

--- driver.java
+++ driver.java
@@ -1,7 +1,7 @@
 public class driver {
   public static void main( String[] args) {
     if ( args.length < 3)
-    System.out.println("Bat formatting; java Driver [rows cols filename [randomSeed [key]]], square brackets denotes optional");
+    System.out.println("Bad formatting; java WordSearch [rows cols filename [randomSeed [key]]], square brackets denotes optional");
     if ( args.length == 3) {
       try {
         int rows = Integer.parseInt( args[0]);
@@ -14,7 +14,7 @@ public class driver {
         WordSearch WSe = new WordSearch( rows, cols, filename);
         System.out.println( WSe);
       } catch( NumberFormatException e) {
-        System.out.println("Parameters rows and cols must be numbers");
+        System.out.println("Parameters rows and cols must be integers");
       }
     }
     if ( args.length == 4) {
@@ -27,14 +27,18 @@ public class driver {
       }
         String filename = args[2];
         int seed = Integer.parseInt( args[3]);
+        if ( !( seed >= 0 && seed <= 10000)) {
+          System.out.println( "seed musts be 0 to 10000 inclusive");
+          System.exit(1);
+        }
         WordSearch WSe = new WordSearch( rows, cols, filename, seed);
         WSe.fill();
         System.out.println( WSe);
       } catch( NumberFormatException e) {
-        System.out.println("Parameters rows, cols, and seed must be numbers");
+        System.out.println("Parameters rows, cols, and seed must be integers");
       }
     }
-    if ( args.length == 5) {
+    if ( args.length >= 5) {
       try {
         int rows = Integer.parseInt( args[0]);
         int cols = Integer.parseInt( args[1]);
@@ -44,12 +48,16 @@ public class driver {
       }
         String filename = args[2];
         int seed = Integer.parseInt( args[3]);
+        if ( !( seed >= 0 && seed <= 10000)) {
+          System.out.println( "seed musts be 0 to 10000 inclusive");
+          System.exit(1);
+        }
         WordSearch WSe = new WordSearch( rows, cols, filename, seed);
         if (!( args[4].equals("key")))
         WSe.fill();
         System.out.println( WSe);
         }catch( NumberFormatException e) {
-          System.out.println("Parameters rows,cols,and seed must be numbers");
+          System.out.println("Parameters rows,cols,and seed must be integers");
         }
       }
     }
